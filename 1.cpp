18a83ea49a7180dc746033a527803bd47f3f49e5
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-07-08 10:44:55 +0530

    cpp lab

--- 1.cpp
+++ 1.cpp
@@ -1,3 +1,20 @@
-// Template in Cpp
+// Template 1 - in Cpp
 #include<iostream>
-
+using namespace std;
+template <class T>
+class num{
+    private: T a;
+    public: num(T x){
+        a = x;
+    }
+    void display(){
+        cout<<a<<endl;
+    }
+};
+int main(){                                 //execution starts from the main func.
+    num<int>obj1(10);                       //num mein ek integer object made and called.
+    obj1.display();                         //another func. called to display it.
+    num<float>obj2(1.1);                    //same things with another object 2.
+    obj2.display();
+    return 0;
+} 
